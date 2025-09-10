9aaffce2848afb3da290e4445330317270d75db1
Rodrigues Lima <135235806+Rodriguesmath@users.noreply.github.com>
2025-09-10 00:50:34 -0300

    Add main class commentary for simulation testing

--- src/main/java/br/com/simulador/controller/Main.java
+++ src/main/java/br/com/simulador/controller/Main.java
@@ -1,7 +1,7 @@
 package main.java.br.com.simulador.controller;
 
 import main.java.br.com.simulador.controller.Controller;
-
+// classe criada para testar o código (main)
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller("Simulador-Hidrometro/config/config.txt");
